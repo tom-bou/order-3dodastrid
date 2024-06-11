c82d97be53b77143bf19b40db33f0af67a702923
tom-bou <tomboustedt@gmail.com>
2024-06-11 04:22:57 +0200

    Fixes to samples and firebase

--- src/Form.js
+++ src/Form.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Select from 'react-select';
+import { db } from './firebase'; // Ensure this path matches your actual file structure
+import { collection, addDoc } from 'firebase/firestore';
 
 const categories = [
-  "Choose a profession...",
+  "Your profession",
   "Architect",
   "Priority Architect",
   "Store",
@@ -24,9 +26,7 @@ const categories = [
   "Other"
 ];
 
-// Sample data extracted from the Excel file
 const samples = [
-  'Choose a sample...',
   'Sample Box ASTRID',
   'Sample Collection Alba*',
   'Sample Collection Ancona',
@@ -37,75 +37,131 @@ const samples = [
   'Sample Collection Axel',
   'Sample Collection Bellaria',
   'Sample Collection Benita',
-  'Sample Collection Beta*',
-  'Sample Collection Bouclé*',
-  'Sample Collection Campiglia',
-  'Sample Collection Cartina',
-  'Sample Collection Casale',
-  'Sample Collection Cascia',
-  'Sample Collection Castello',
-  'Sample Collection Chamois',
-  'Sample Collection Cima Recycled',
-  'Sample Collection Cirrus',
-  'Sample Collection Cocoon',
-  'Sample Collection Como',
-  'Sample Collection Conero Recycled',
-  'Sample Collection Costera',
-  'Sample Collection Cross',
-  'Sample Collection Crudo*',
-  'Sample Collection Cyclone',
-  'Sample Collection Domino*',
-  'Sample Collection Donna',
-  'Sample Collection Dry',
-  'Sample Collection Egna',
-  'Sample Collection Elevation',
-  'Sample Collection Escape',
-  'Sample Collection Etamine',
-  'Sample Collection Fantasy*',
-  'Sample Collection Firefly*',
-  'Sample Collection Fusion',
-  'Sample Collection Genua',
-  'Sample Collection Gilda',
-  'Sample Collection Grimaldi',
-  'Sample Collection Lario',
-  'Sample Collection Latina',
-  'Sample Collection Linda',
-  'Sample Collection Linen Look',
-  'Sample Collection Loano',
-  'Sample Collection Lora',
-  'Sample Collection Lykka',
-  'Sample Collection Maestro*',
-  'Sample Collection Mana',
-  'Sample Collection Memento',
-  'Sample Collection Natura',
-  'Sample Collection Nizza*',
-  'Sample Collection Norma',
-  'Sample Collection Nypon*',
-  'Sample Collection Olimpia',
-  'Sample Collection Orelle*',
-  'Sample Collection Pacino',
-  'Sample Collection Pienza',
-  'Sample Collection Recycle wool',
-  'Sample Collection Retro',
-  'Sample Collection Rhytm',
-  'Sample Collection Ria*',
-  'Sample Collection Riva',
-  'Sample Collection Rodeo',
-  'Sample Collection Rovigo',
-  'Sample Collection San Siro',
-  'Sample Collection Stella*',
-  'Sample Collection Surf',
-  'Sample Collection Svezia',
-  'Sample Collection Tellus*',
-  'Sample Collection Verona',
-  'Sample Collection Villa'
+  'Sample Collection Bern',
+  'Sample Collection Bottero',
+  'Sample Collection Brera',
+  'Sample Collection Bucati',
+  'Sample Collection California',
+  'Sample Collection Chalet',
+  'Sample Collection Como/Lago',
+  'Sample Collection Davos',
+  'Sample Collection Della Nonna Neutrals',
+  'Sample Collection Dino',
+  'Sample Collection Dolores Blackout*',
+  'Sample Collection Dorotea/Paolina',
+  'Sample Collection Etna',
+  'Sample Collection Felix',
+  'Sample Collection Firenze',
+  'Sample Collection Fresia',
+  'Sample Collection Harrietta Recycled- New colours 2023',
+  'Sample Collection Heavy Neutrals',
+  'Sample Collection Hedda',
+  'Sample Collection Hemp',
+  'Sample Collection Langhe & Nebbia',
+  'Sample Collection Levanto',
+  'Sample Collection Lucca',
+  'Sample Collection Lugano',
+  'Sample Collection Luna Dimout Recycled',
+  'Sample Collection Malva',
+  'Sample Collection Milano',
+  'Sample Collection Modena MM',
+  'Sample Collection Mons',
+  'Sample Collection Monti & Rialto',
+  'Sample Collection Murano',
+  'Sample Collection Nesso',
+  'Sample Collection- New Color Sheet',
+  'Sample Collection Nicola Recycled',
+  'Sample Collection Nizza/Grasse',
+  'Sample Collection Nora',
+  'Sample Collection Notte Dimout',
+  'Sample Collection Olivia Recycled',
+  'Sample Collection Parma',
+  'Sample Collection Perfect Beige',
+  'Sample Collection Ponza',
+  'Sample Collection Prato',
+  'Sample Collection Ripasso',
+  'Sample Collection Rivoli',
+  'Sample Collection Roma & Udine',
+  'Sample Collection Sablon Washed',
+  'Sample Collection Sabrina Recycled',
+  'Sample Collection Savona',
+  'Sample Collection Selected Neutrals',
+  'Sample Collection Siena',
+  'Sample Collection Stella Dimout',
+  'Sample Collection Toledo',
+  'Sample Collection Torno',
+  'Sample Collection Trentina/Torino',
+  'Sample Collection Udine WIP',
+  'Sample Collection Valencia',
+  'Sample Collection Varese',
+  'Sample Collection Venere Mano Morbida',
+  'Sample Collection Vittoria Recycled',
+  'Sample Collection Wool Collection',
+  'Sample Collection Wool Collection II',
+  'Sample Collection Yoko',
+  'Sample Sheet'
 ].map(sample => ({ label: sample, value: sample }));
 
-function Form({ formData }) {
+function Form() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phoneNumber: '',
+    countryCode: '',
+    company: '',
+    organizationNumber: '',
+    country: '',
+    city: '',
+    category: '',
+    samples: [],
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+
+  const handleSelectChange = (selectedOptions) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      samples: selectedOptions ? selectedOptions.map(option => option.value) : []
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const docRef = await addDoc(collection(db, "Orders"), formData);
+      console.log("Document written with ID: ", docRef.id);
+      alert('Form submitted successfully');
+      // Reset form if needed
+      setFormData({
+        name: '',
+        email: '',
+        phoneNumber: '',
+        countryCode: '',
+        company: '',
+        organizationNumber: '',
+        country: '',
+        city: '',
+        category: '',
+        samples: [],
+        message: ''
+      });
+    } catch (e) {
+      console.error("Error adding document: ", e);
+      alert('Error submitting form');
+    }
+  };
+
   return (
     <div>
-      <h2 className="text-2xl font-bold mb-6 text-gray-800">Textile Fair Registration</h2>
-      <form className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <h2 className="text-2xl font-bold mb-6 text-gray-800">Astrid Sample Order</h2>
+      <form className="grid grid-cols-1 md:grid-cols-2 gap-4" onSubmit={handleSubmit}>
         <div className="mb-4 md:col-span-1">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
             Name
@@ -115,7 +171,9 @@ function Form({ formData }) {
             id="name"
             type="text"
             placeholder="Your name"
-            readOnly
+            value={formData.name}
+            onChange={handleChange}
+            autoComplete="name"
           />
         </div>
         <div className="mb-4 md:col-span-1">
@@ -127,7 +185,9 @@ function Form({ formData }) {
             id="email"
             type="email"
             placeholder="Your email"
-            readOnly
+            value={formData.email}
+            onChange={handleChange}
+            autoComplete="email"
           />
         </div>
         <div className="mb-4 md:col-span-2">
@@ -140,14 +200,18 @@ function Form({ formData }) {
               id="countryCode"
               type="text"
               placeholder="+46"
-              readOnly
+              value={formData.countryCode}
+              onChange={handleChange}
+              autoComplete="tel-country-code"
             />
             <input
               className="shadow appearance-none border rounded-r w-4/5 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="phoneNumber"
               type="tel"
               placeholder="Your phone number"
-              readOnly
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              autoComplete="tel-national"
             />
           </div>
         </div>
@@ -160,7 +224,23 @@ function Form({ formData }) {
             id="company"
             type="text"
             placeholder="Your company"
-            readOnly
+            value={formData.company}
+            onChange={handleChange}
+            autoComplete="organization"
+          />
+        </div>
+        <div className="mb-4 md:col-span-1">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="organizationNumber">
+            Organization Number
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="organizationNumber"
+            type="text"
+            placeholder="Your organization number"
+            value={formData.organizationNumber}
+            onChange={handleChange}
+            autoComplete="off"
           />
         </div>
         <div className="mb-4 md:col-span-1">
@@ -172,7 +252,9 @@ function Form({ formData }) {
             id="country"
             type="text"
             placeholder="Your country"
-            readOnly
+            value={formData.country}
+            onChange={handleChange}
+            autoComplete="country"
           />
         </div>
         <div className="mb-4 md:col-span-1">
@@ -184,17 +266,21 @@ function Form({ formData }) {
             id="city"
             type="text"
             placeholder="Your city"
-            readOnly
+            value={formData.city}
+            onChange={handleChange}
+            autoComplete="address-level2"
           />
         </div>
         <div className="mb-4 md:col-span-1">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="workTitle">
-            Category
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="category">
+            Profession
           </label>
           <select
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="workTitle"
-            readOnly
+            id="category"
+            value={formData.category}
+            onChange={handleChange}
+            autoComplete="off"
           >
             {categories.map((category) => (
               <option key={category} value={category}>
@@ -213,6 +299,8 @@ function Form({ formData }) {
             isMulti
             isClearable
             placeholder="Select a sample..."
+            value={samples.filter(option => formData.samples.includes(option.value))}
+            onChange={handleSelectChange}
           />
         </div>
         <div className="mb-4 md:col-span-2">
@@ -223,12 +311,14 @@ function Form({ formData }) {
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="message"
             placeholder="Your message"
+            value={formData.message}
+            onChange={handleChange}
           ></textarea>
         </div>
         <div className="flex items-center justify-between md:col-span-2">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
+            type="submit"
           >
             Submit
           </button>
